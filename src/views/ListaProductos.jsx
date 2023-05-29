e71364dc58b4dc4e350fcc369ecdafb3b51e58f2
Walter Thumann <wthumann@gmail.com>
2023-05-29 18:51:17 -0400

    quitar console logs

--- src/views/ListaProductos.jsx
+++ src/views/ListaProductos.jsx
@@ -4,18 +4,21 @@ import ProductoCard from "../components/producto-components/ProductoCard";
 // import productos from "../productos";
 import "../styles/productCard.css";
 import axios from "axios";
+import { Spinner } from "react-bootstrap";
 
 const ListaProductos = () => {
+  const [loading, setLoading] = useState(true);
   const [productos, setProductos] = useState([]);
 
   useEffect(() => {
     const obtenerProductos = async () => {
       try {
-        const urlServer = process.env.BACKEND_URL;
+        const urlServer = process.env.REACT_APP_BACKEND_URL;
         const endpoint = "/productos";
 
         const { data: productos } = await axios.get(urlServer + endpoint);
         setProductos(productos);
+        setLoading(false);
       } catch (error) {
         console.log({ error });
       }
@@ -25,12 +28,16 @@ const ListaProductos = () => {
 
   return (
     <Container>
-      <section className="lista-productos">
-        {productos &&
-          productos.map((producto) => {
-            return <ProductoCard key={producto.id} producto={producto} />;
-          })}
-      </section>
+      {loading ? (
+        <Spinner animation="border" variant="success" />
+      ) : (
+        <section className="lista-productos">
+          {productos &&
+            productos.map((producto) => {
+              return <ProductoCard key={producto.id} producto={producto} />;
+            })}
+        </section>
+      )}
     </Container>
   );
 };
